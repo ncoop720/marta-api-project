462ed0b193a9da3107fe0a738081dce99fc330a6
Nathan Cooper <ncoop720@yahoo.com>
2018-05-30 18:48:10 -0400

    first commit

---
+++ app/models/location.rb
@@ -0,0 +1,13 @@
+class Location < ApplicationRecord
+
+	validates :address, presence: true
+  validates :city, presence: true
+
+	geocoded_by :my_location
+  after_validation :geocode
+
+  def my_location
+    "#{address}, #{city}, GA"
+  end
+  
+end
